280958db11ec0da39dd93662e331a71831432943
Francisco José Rodríguez Bogado <bogado@qinn.es>
2021-03-01 10:29:15 +0100

    sqlite to mysql due incompatibility in my hosting.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,13 +77,24 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': secrets.DBNAME,
+        'USER': secrets.DBUSER,
+        'PASSWORD': secrets.DBPASSWORD,
+        'HOST': secrets.DBHOST,
+        'PORT': secrets.DBPORT,
+        'OPTIONS': {'sql_mode': 'STRICT_TRANS_TABLES'},
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
