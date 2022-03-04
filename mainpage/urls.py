ce3ba747212ed319ba16dde9263909a1666a795e
Francisco José Rodríguez Bogado <bogado@qinn.es>
2022-03-04 15:36:11 +0100

    Static sitemap.

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from .views import ProjectListBadgesAndFormView
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', ProjectListBadgesAndFormView.as_view(), name='main')
+    url(r'^$', ProjectListBadgesAndFormView.as_view(), name='main'),
+    url(r'^sitemap\.xml$', TemplateView.as_view(template_name='mainpage/sitemap.xml', content_type='text/xml')),
 ]
